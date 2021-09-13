dfbaab357908e4eaff89db8ca47a67a3e5553d43
Kerwin joel <joelss4029@gmail.com>
2021-09-13 16:08:00 -0500

    make it better

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/order")
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT})
 public class OrderController {
     @Autowired
     OrderService orderService;
