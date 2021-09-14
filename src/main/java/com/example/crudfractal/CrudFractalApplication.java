5658489526192d301c78fa19fa11a136adc7cafb
Kerwin joel <joelss4029@gmail.com>
2021-09-14 01:29:11 -0500

    add field quantity to entity

--- src/main/java/com/example/crudfractal/CrudFractalApplication.java
+++ src/main/java/com/example/crudfractal/CrudFractalApplication.java
@@ -2,9 +2,12 @@ package com.example.crudfractal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories("com.example.repository")
@@ -13,6 +16,18 @@ public class CrudFractalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrudFractalApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 
 }
